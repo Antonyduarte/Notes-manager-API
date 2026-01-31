0b513c95fb7756202f136d4bd106ffbf8111310f
Antony <166068959+Antonyduarte@users.noreply.github.com>
2026-01-31 14:40:10 -0300

    Fix PUT update note route

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 // require const's
-const dotenv = require("dotenv").config()
+require("dotenv").config()
 const express = require("express")
 const mysql = require("mysql2")
 const cors = require("cors")
@@ -45,7 +45,7 @@ app.get("/note/:id", (req, res) => {
     connection.query("SELECT * FROM notes WHERE id = ?", [id], (err, rows) => {
         if (!err) {
             if (rows.length > 0) {
-                return res.json(defs.response("Success", "Task found", rows.affectedRows, rows))
+                return res.json(defs.response("Success", "Note found", rows.length, rows))
             } else {
                 return res.status(404).json(defs.response("Error", "Task not found", 0, null))
             }
@@ -54,6 +54,7 @@ app.get("/note/:id", (req, res) => {
         }
     })
 })
+//search note by NOTE
 app.get("/note/search/:note", (req, res) => {
     const note = String(req.params.note)
     if (!note || note.trim() === "") {
@@ -96,21 +97,21 @@ app.post("/note/create", (req, res) => {
 app.put("/note/edit/:id", (req, res) => {
     const postData = req.body
     const title = postData.title
+    const note = postData.note 
+    const id = req.params.id
     if (!title || title.trim() === "") {
         return res.status(400).json(defs.response("Error", "Title can not be empty", 0, null))
     }
-    const note = postData.note
     if (!note || note.trim() === "") {
         return res.status(400).json(defs.response("Error", "Note must be a content", 0, null))
     }
-    const id = req.params.id
     // query
-    connection.query("UPDATE notes SET note = ?, modified_at = NOW() WHERE id = ?", [note, id], (err, result) => {
+    connection.query("UPDATE notes SET title = ?, note = ? WHERE id = ?", [title, note, id], (err, result) => {
         if (!err) {
             if (result.affectedRows > 0) {
                 res.json(defs.response("Success", "Note sussefully updated", result.affectedRows, result))
             } else {
-                return res.status(404).json(defs.response("error", "ID not found"), 0, null)
+                return res.status(404).json(defs.response("Error", "ID not found", 0, null))
             }
         } else {
             return res.status(500).json(defs.response("Internal error", err.message, 0, null))
@@ -151,5 +152,3 @@ app.delete("/note/delete/:id", (req, res) => {
 app.use((req, res) => {
     res.status(404).json(defs.response("Error", "Route NOT found", 0, null))
 })
-
-
